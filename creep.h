6776eb32d737adca26b97475173a5d8d74485301
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2015-02-20 00:53:51 +0100

    Make creeping speed available to the rest of the code

--- creep.h
+++ creep.h
@@ -1,6 +1,11 @@
 #ifndef CREEP_H
 #define CREEP_H
 
+/* Walking speed. */
+extern double creep_dx; // Sideways.
+extern double creep_dy; // Forward.
+
+/* Perform one step of the creep gait with the current speed. */
 void creep();
 
 #endif
