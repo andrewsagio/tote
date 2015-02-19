e2d6f7e0803371c4aa2791bffd7431f7bced09c0
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2015-02-20 00:07:13 +0100

    Crawl with body shifting

--- creep.ino
+++ creep.ino
@@ -3,19 +3,22 @@
 #include "leg.h"
 #include "clock.h"
 
-#define STEP_HEIGHT 16.0
-#define STRIDE 12.0
-#define HOME ((COXA + FEMUR) / SQRT2)
+#define STEP_HEIGHT 15.0
+#define STRIDE 18.0
+#define SHIFT 6.0
+#define STEPS 4
+#define HOME ((COXA + FEMUR) / SQRT2) * 0.75
+#define BODY_HEIGHT TIBIA
 
 const unsigned char _NEXT_LEG[4] = {1, 3, 0, 2};
 
 
-// static double _shift_x = 0.0;
-// static double _shift_y = 0.0;
+static double _shift_x = 0.0;
+static double _shift_y = 0.0;
 static bool _on_ground[4] = {true, true, true, true};
 
 double creep_dx = 0;
-double creep_dy = 2.0;
+double creep_dy = 1.0;
 
 
 void _creep_move() {
@@ -36,10 +39,14 @@ void _raise_leg(unsigned char leg) {
     _creep_tick();
     _on_ground[leg] = false;
     _creep_tick();
+    _creep_tick();
+    _creep_tick();
 }
 
 void _lower_leg(unsigned char leg) {
     move_leg_by(leg, 0, 0, -STEP_HEIGHT);
+    _creep_tick();
+    _creep_tick();
     _creep_tick();
     _on_ground[leg] = true;
     _creep_tick();
@@ -48,9 +55,9 @@ void _lower_leg(unsigned char leg) {
 void _advance_leg(unsigned char leg) {
     move_leg(
         leg,
-        HOME + creep_dx * STRIDE * LEG_X[leg],
-        HOME + creep_dy * STRIDE * LEG_Y[leg],
-        leg_position[leg][2]
+        HOME + (_shift_x + creep_dx * STRIDE) * LEG_X[leg],
+        HOME + (_shift_y + creep_dy * STRIDE) * LEG_Y[leg],
+        -BODY_HEIGHT
     );
     _creep_tick();
     _creep_tick();
@@ -58,7 +65,25 @@ void _advance_leg(unsigned char leg) {
     _creep_tick();
 }
 
+void _shift_body_by(double dx, double dy) {
+    for (unsigned char leg=0; leg < 4; ++leg) {
+        move_leg_by(leg, dx * LEG_X[leg], dy * LEG_Y[leg], 0);
+    }
+    _shift_x += dx;
+    _shift_y += dy;
+}
+
+void _shift_body(unsigned char leg) {
+    double dx = (LEG_X[leg] * SHIFT - _shift_x) / STEPS;
+    double dy = (LEG_Y[leg] * SHIFT - _shift_y) / STEPS;
+    for (unsigned char i = 0; i < STEPS; ++i) {
+        _shift_body_by(dx, dy);
+        _creep_tick();
+    }
+}
+
 void _creep_step(unsigned char leg) {
+    _shift_body(leg);
     _raise_leg(leg);
     _advance_leg(leg);
     _lower_leg(leg);
