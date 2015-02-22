c4df210a9f88ce43e444b23d879f962f90284b24
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2015-02-22 15:04:17 +0100

    Add more buttons

--- dice/ir.ino
+++ dice/ir.ino
@@ -1,4 +1,6 @@
 #include "creep.h"
+#include "leg.h"
+#include "servos.h"
 #include "misc.h"
 #include <IRLremote.h>
 
@@ -24,21 +26,38 @@ void IREvent(uint8_t protocol, uint16_t address, uint32_t command) {
         command = last;
     }
     switch (command) {
-        case 0xBF40:
+        case 0xBF40:    // Up arrow.
             creep_dy = min(1.0, creep_dy + 0.25);
             break;
-        case 0xE619:
+        case 0xE619:    // Down arrow.
             creep_dy = max(-1.0, creep_dy - 0.25);
             break;
-        case 0xF807:
-            //creep_dx = min(1.0, creep_dx + 0.25);
+        case 0xF807:    // Left arrow.
             creep_rotation = min(PI/90, creep_rotation + PI/360);
             break;
-        case 0xF609:
-            //creep_dx = max(-1.0, creep_dx - 0.25);
+        case 0xF609:    // Right arrow.
             creep_rotation = max(-PI/90, creep_rotation - PI/360);
             break;
-        case 0xEA15:
+        case 0xF30C:    // Rewind.
+            creep_dx = max(-1.0, creep_dx - 0.25);
+            break;
+        case 0xA15E:    // Fast forward.
+            creep_dx = min(1.0, creep_dx + 0.25);
+            break;
+        case 0xAF50:    // Power.
+            servo_shutdown();
+            break;
+        case 0xB847:    // Home.
+            for (unsigned char leg=0; leg < 4; ++leg) {
+                move_leg(
+                    leg,
+                    ((COXA + FEMUR) / SQRT2),
+                    ((COXA + FEMUR) / SQRT2),
+                    TIBIA
+                );
+            }
+            break;
+        case 0xEA15:    // OK.
             creep_dx = 0.0;
             creep_dy = 0.0;
             creep_rotation = 0.0;
