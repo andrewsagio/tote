88db24425b198d23718a637b4c290052830e3017
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2015-02-28 23:25:27 +0100

    Add missing files

---
+++ main/main.ino
@@ -0,0 +1,30 @@
+#include "servos.h"
+#include "beep.h"
+#include "battery.h"
+#include "creep.h"
+#include "leg.h"
+#include "misc.h"
+
+#define HOME ((COXA + FEMUR) / SQRT2)
+
+bool power = true;
+
+
+void setup() {
+    battery_setup();
+    servo_setup();
+    beep_setup();
+    ir_setup();
+}
+
+void ir_loop();
+
+void loop() {
+    if (power) {
+        creep();
+        ir_loop();
+    } else {
+        servo_shutdown();
+        while (!power) delay(100);
+    }
+}
