064e10dc24c62a774d89f3d09ee1f245bf6328df
Radomir Dopieralski <sheep-devel@sheep.art.pl>
2015-05-02 17:51:38 +0200

    Define ir key codes

--- start/ir.ino
+++ start/ir.ino
@@ -11,6 +11,26 @@
 #define IR_PROTOCOL IR_NEC
 #define IR_ADDRESS 0xFF00
 
+#define KEY_UP      0xBF40 // Up arrow.
+#define KEY_DOWN    0xE619 // Down arrow.
+#define KEY_LEFT    0xF807 // Left arrow.
+#define KEY_RIGHT   0xF609 // Right arrow.
+#define KEY_REV     0xF30C // Rewind.
+#define KEY_PLAY    0xE718 // Play/pause.
+#define KEY_FF      0xA15E // Fast forward.
+#define KEY_CHMINUS 0xF708 // Channel minus.
+#define KEY_CHPLUS  0xA55A // Channel plus.
+#define KEY_CHLEVEL 0xE31C
+#define KEY_VOLDOWN 0xBD42 // Volume down.
+#define KEY_MUTE    0xAD52 // Mute.
+#define KEY_VOLUP   0xB54A // Volume up.
+#define KEY_POWER   0xBA45 // Power.
+#define KEY_POWER2  0xAF50 // Power.
+#define KEY_SEARCH  0xBB44 // Search.
+#define KEY_HOME    0xB847 // Home.
+#define KEY_OK      0xEA15 // OK.
+
+
 extern bool power;
 uint32_t IRcommand = 0;
 
@@ -41,52 +61,52 @@ void ir_loop() {
     digitalWrite(13, HIGH);
 
     switch (IRcommand) {
-        case 0xBF40:    // Up arrow.
+        case KEY_UP:    // Up arrow.
             creep_dy = min(1.5, creep_dy + 0.25);
             break;
-        case 0xE619:    // Down arrow.
+        case KEY_DOWN:    // Down arrow.
             creep_dy = max(-1.5, creep_dy - 0.25);
             break;
-        case 0xF807:    // Left arrow.
+        case KEY_LEFT:    // Left arrow.
             creep_rotation = min(PI/90, creep_rotation + PI/360);
             break;
-        case 0xF609:    // Right arrow.
+        case KEY_RIGHT:    // Right arrow.
             creep_rotation = max(-PI/90, creep_rotation - PI/360);
             break;
-        case 0xF30C:    // Rewind.
+        case KEY_REV:    // Rewind.
             creep_spread = max(-10.0, creep_spread - 1);
             break;
-        case 0xE718:    // Play/pause.
+        case KEY_PLAY:    // Play/pause.
             creep_spread = 0.0;
             break;
-        case 0xA15E:    // Fast forward.
+        case KEY_FF:    // Fast forward.
             creep_spread = min(10.0, creep_spread + 1);
             break;
-        case 0xF708:    // Channel minus.
+        case KEY_CHMINUS:    // Channel minus.
             creep_height = max(TIBIA * 0.25, creep_height - 1);
             break;
-        case 0xA55A:    // Channel plus.
+        case KEY_CHPLUS:    // Channel plus.
             creep_height = min(TIBIA * 1.25, creep_height + 1);
             break;
-        case 0xE31C:
+        case KEY_CHLEVEL:
             creep_height = TIBIA * 0.75;
             break;
-        case 0xBD42:    // Volume down.
+        case KEY_VOLDOWN:    // Volume down.
             creep_dx = max(-1.0, creep_dx - 0.25);
             break;
-        case 0xAD52:    // Mute.
+        case KEY_MUTE:    // Mute.
             beep_mute = !beep_mute;
             break;
-        case 0xB54A:    // Volume up.
+        case KEY_VOLUP:    // Volume up.
             creep_dx = min(1.0, creep_dx + 0.25);
             break;
-        case 0xBA45:    // Power.
-        case 0xAF50:    // Power.
+        case KEY_POWER:    // Power.
+        case KEY_POWER2:    // Power.
             power = !power;
             break;
-        case 0xBB44:    // Search.
+        case KEY_SEARCH:    // Search.
             break;
-        case 0xB847:    // Home.
+        case KEY_HOME:    // Home.
             for (unsigned char leg=0; leg < 4; ++leg) {
                 move_leg(
                     leg,
@@ -96,14 +116,13 @@ void ir_loop() {
                 );
             }
             break;
-        case 0xEA15:    // OK.
+        case KEY_OK:    // OK.
             creep_dx = 0.0;
             creep_dy = 0.0;
             creep_rotation = 0.0;
             break;
     }
     IRcommand = 0;
-
 }
 
 void IREvent(uint8_t protocol, uint16_t address, uint32_t command) {
